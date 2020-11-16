002cfbb8451fefaf34f7c1353a1062462a70c7c0
tetema <tetema@student.wethinkcode.co.za>
2020-11-16 10:58:37 +0200

    Refactored the code, added docstrings to the code, and added some functionality to the code

---
+++ code-clinics-iter-01/process.py
@@ -0,0 +1,17 @@
+"""
+Come up with a better name....
+This module basically carries out the functionality for commands
+book, slots, volunteer and cancel.
+"""
+
+
+def get_slots():
+    """
+    This will get all the available slots form the WTC calendar. 
+    """
+    #possibly returns all slots booked and open
+    pass
+
+
+def open_slots():
+    pass
