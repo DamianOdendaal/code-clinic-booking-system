0ce700a3e539d8160c48eeb47440cba7abbd2f81
rbrummer <rbrummer@student.wethinkcode.co.za>
2020-11-19 10:47:36 +0200

    Calendar events can now be created with user input. Error handling required.

--- codeclinics/slots/create_event.py
+++ codeclinics/slots/create_event.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import timedelta
 from calendar_setup import get_calendar_service
 
 
@@ -27,19 +28,33 @@ def main():
     for i in dates:
         print('\t' + i, dates[i], print_day(dates[i].weekday()+1), sep=' : ')
     
-    try:
-        d = input('\nPlease choose the date of a day that you would like to volunteer on\n\n\tDate : ')
-        print('\n')
-       
-        time = input('Please specify a time between 07:00-17:00 at which you will avail yourself for 30 minutes\n\n\tHH:MM : ')
-        print('\n')
-       
-        start = datetime.datetime.strptime(str(dates[d])+'T'+time, '%Y-%m-%dT%H:%M').isoformat()
-    
-    except KeyError as k_err:
-        print('Please enter one of the available dates.')
-    except ValueError:
-        print('Please use a \':\' between the hours and minutes.\n')
+    # try:
+    d = input('\nPlease choose the date of a day that you would like to volunteer on\n\n\tDate : ')
+    print('\n')
+
+    time = input('Please specify a time between 07:00-17:00 at which you will avail yourself for 30 minutes\n\n\tHH:MM : ')
+    print('\n')
+
+    start = datetime.datetime.strptime(str(dates[d])+'T'+time, '%Y-%m-%dT%H:%M').isoformat()
+    # except KeyError as key_err:
+    #     while True:
+    #         if key_err:
+    #             print('Please enter one of the available dates.')
+    #             d = input('\nPlease choose the date of a day that you would like to volunteer on\n\n\tDate : ')
+    #             print('\n')
+    #             continue
+    #         elif not key_err:
+    #             break
+    # except ValueError as val_err:
+    #     while True:
+    #         if val_err:
+    #             time = input('Please specify a time between 07:00-17:00 at which you will avail yourself for 30 minutes\n\n\tHH:MM : ')
+    #             print('\n')
+    #             print('Please use a \':\' between the hours and minutes.\n')
+    #             continue
+    #         elif not val_err:
+    #             break
+
 
     summary = input('Please name the concept you are offering help with\n\n\tTopic : ')
     print('\n')
@@ -47,50 +62,52 @@ def main():
     description = input('Please add a description of the concept you are offering help with\n\n\tDescription : ')
     print('\n')
 
-    print('Topic: ' + summary)
-    print('Description: ' + description)
+    print('Topic :' + summary)
+    print('Description :' + description)
     # print()
     print(start)
 
 
     # creates one hour event tomorrow 10 AM GMT
-    # service = get_calendar_service()
+    service = get_calendar_service()
     
+    # d = datetime.now().date()
     # tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
     # start = tomorrow.isoformat()
-    # end = (tomorrow + timedelta(minutes=30)).isoformat()
+    start_obj = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S')
+    end = (start_obj + timedelta(minutes=30)).isoformat()
 
 
-    # event_result = service.events().insert(calendarId='primary',
-    #     body={
-    #         "summary": summary,
-    #         "description": description,
-    #         "start": {"dateTime": start, "timeZone": 'Africa/Johannesburg'},
-    #         "end": {"dateTime": end, "timeZone": 'Africa/Johannesburg'},
+    event_result = service.events().insert(calendarId='primary',
+        body={
+            "summary": summary,
+            "description": description,
+            "start": {"dateTime": start, "timeZone": 'Africa/Johannesburg'},
+            "end": {"dateTime": end, "timeZone": 'Africa/Johannesburg'},
             
-    #         # Adds the 'attendance UI' on the user's calendar
-    #         # 'attendees': [{
-    #         #   'email': 'rbrummer@student.wethinkcode.co.za'  
-    #         # }],
+            # Adds the 'attendance UI' on the user's calendar
+            # 'attendees': [{
+            #   'email': 'rbrummer@student.wethinkcode.co.za'  
+            # }],
 
-    #         'scope': {
-    #             # visibility property of the event
-    #             'visibility': 'default',
-    #             # limits the scope to a single user
-    #             'type': 'user',
-    #             # the email address of a user, group or domain
-    #             'value': 'rbrummer@student.wethinkcode.co.za',
-    #         },
-    #         # the type of access the user receives on the events
-    #         'role': 'reader'
-    #     }
-    # ).execute()
+            'scope': {
+                # visibility property of the event
+                'visibility': 'public',
+                # limits the scope to a single user
+                'type': 'default',
+                # the email address of a user, group or domain
+                'value': 'rbrummer@student.wethinkcode.co.za',
+            },
+            # the type of access the user receives on the events
+            'role': 'reader'
+        }
+    ).execute()
 
-    # print("created event")
-    # print("id: ", event_result['id'])
-    # print("summary: ", event_result['summary'])
-    # print("starts at: ", event_result['start']['dateTime'])
-    # print("ends at: ", event_result['end']['dateTime'])
+    print("created event")
+    print("id: ", event_result['id'])
+    print("summary: ", event_result['summary'])
+    print("starts at: ", event_result['start']['dateTime'])
+    print("ends at: ", event_result['end']['dateTime'])
 
 if __name__ == "__main__":
     main()
