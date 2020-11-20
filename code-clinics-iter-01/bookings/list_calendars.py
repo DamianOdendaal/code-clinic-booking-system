3004e6d89289bc2f227bd31ce2ab15ea100343c3
tetema <tetema@student.wethinkcode.co.za>
2020-11-20 13:15:34 +0200

    Added the view calendar functionality

--- code-clinics-iter-01/bookings/list_calendars.py
+++ code-clinics-iter-01/bookings/list_calendars.py
@@ -1,6 +1,8 @@
 from calendar_setup import get_calendar_service
 from datetime import datetime, timedelta
-from pprint import pprint
+from tabulate import tabulate
+from prettytable import PrettyTable
+from termcolor import colored
 
 # Fetching the Google Calendar API
 service = get_calendar_service()
@@ -27,29 +29,96 @@ def date_and_time_str_google():
     return date
 
 
-def show_student_calendar():
-    """Displaying 7 days of events from the student's calendar."""
+def colored_headings():
+    """Defining variables for the Slot Grid template."""
+    
+    colors = {
+        "date_header": colored("DATE", 'green'),
+        "time_header": colored("TIME", 'green'),
+        "summary": colored("SUMMARY", 'cyan'),
+        "volunteer": colored("VOLUNTEER", 'red'),
+        "patient": colored("PATIENT", "magenta"),
+        "etag": colored("ID", "yellow"),
+        "booked": colored("[BOOKED]", "green"),
+        "available": colored("[OPEN]", "cyan"),
+        "canceled": colored("[CANCELED]", "red")
+    }
 
+    return colors
+
+
+def show_code_clinics_calendar():
+    """ """
+
+    colors = colored_headings()
     date = date_and_time_str_google()
-    events_result = service.events().list(calendarId='primary',
-        timeMin=date.get('start_day'), timeMax=date.get('end_day'), 
+
+    events_result = service.events().list(calendarId='wtcteam19jhb@gmail.com',
+        timeMin=date.get('start_day'), timeMax=date.get('end_day'),
         maxResults=100, singleEvents=True, orderBy='startTime').execute()
-    events = events_result.get('items')
 
+    events = events_result.get('items', [])
+
+    data = []
     if not events:
-        print('No upcoming events found!')
+        print("No upcoming events found!")
 
-    print('Your next 7 days upcoming events:\n')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        
+        patient = "--"
+
         start_date = start.split('T')[0]
         start_time_UCT = start.split('+')[0].split('T')[1]
+
+        data.append([start_date, start_time_UCT, event['summary'], patient, 
+            event['creator'].get('email'),
+            event['id']])
+
+    return data
+
+
+def get_slots_calendar():
+
+    colors = colored_headings()
+    data = show_code_clinics_calendar()
+
+    table = PrettyTable()
+    table.field_names = [colors.get("date_header"), colors.get("time_header"),
+        colors.get("summary"), colors.get("patient"), colors.get("volunteer"), 
+        colors.get("etag")]
+
+    for entry in data:
+        table.add_row(entry)
+
+    print(table)
+
+
+def view_student_calendar():
+
+    colors = colored_headings()
+    
+    
+# def show_student_calendar():
+#     """Displaying 7 days of events from the student's calendar."""
+
+#     date = date_and_time_str_google()
+#     events_result = service.events().list(calendarId='wtcteam19jhb@gmail.com',
+#         timeMin=date.get('start_day'), timeMax=date.get('end_day'), 
+#         maxResults=100, singleEvents=True, orderBy='startTime').execute()
+#     events = events_result.get('items')
+
+#     if not events:
+#         print('No upcoming events found!')
+
+#     print('Your next 7 days upcoming events:\n')
+#     for event in events:
+        # start = event['start'].get('dateTime', event['start'].get('date'))
         
-        print(f"{start_date} | {start_time_UCT} | {event['summary']}")
-        # start_date = start.split('T')[1]
+        # start_date = start.split('T')[0]
+        # start_time_UCT = start.split('+')[0].split('T')[1]
         
-        # print(f"{start} || {event['summary']}")
+#         print(f"{start_date} | {start_time_UCT} | {event['summary']}")
+
 
 if __name__ == "__main__":
-    show_student_calendar()
+    get_slots_calendar()
--- code-clinics-iter-01/calendar_setup.py
+++ code-clinics-iter-01/calendar_setup.py
@@ -8,7 +8,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 def get_calendar_service():
