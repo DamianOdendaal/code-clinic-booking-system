90b6acde8d16f2c7630c0478ac8c55d25a0460c4
tsoulo <tsoulo@student.wethinkcode.co.za>
2020-12-12 11:53:00 +0200

    updates

--- codeclinics/bookings/list_calendars.py
+++ codeclinics/bookings/list_calendars.py
@@ -1,206 +1,222 @@
-
-from calendar_setup import get_calendar_service
-from datetime import datetime, timedelta
-from tabulate import tabulate
+from random import randint
+from calendar_setup.calendar_service import *
 from prettytable import PrettyTable
 from termcolor import colored
-
-from login import user_auth as user
-
-if user.get_login_state():
-   service = get_calendar_service() 
-# Fetching the Google Calendar API
-
-def date_and_time_str_google():
-    """Converting datetime python data format into a date string that matches 
-    the google time format. 
-    This function returns a dictionary of date and time."""
-
-    #service = get_calendar_service()
-    raw_date_min = datetime.now()
-    now_split = str(raw_date_min).split(" ")
-    now = now_split[0] + 'T' + now_split[1] + 'Z'
-
-    raw_date_max = raw_date_min + timedelta(6)
-    day_7_split = str(raw_date_max).split(" ")
-    day_7 = day_7_split[0] + 'T' + day_7_split[1] + 'Z'
-
-    date = {
-        "start_day": now,
-        "end_day": day_7
-    }
-
-    return date
+from dateutil import parser
+from bookings.processing_data import *
+import json
+import sys
+from datetime import datetime, timedelta
+from dateutil import parser
 
 
-def colored_headings():
-    """Defining variables for the Slot Grid template."""
-    #service = get_calendar_service()
-    colors = {
-        "date_header": colored("DATE", 'green'),
-        "time_header": colored("TIME", 'yellow'),
-        "summary": colored("SUMMARY", 'cyan'),
-        "volunteer": colored("VOLUNTEER", 'red'),
-        "etag": colored("ID", "yellow"),
-        "booked": colored("[BOOKED]", "red"),
-        "available": colored("[OPEN]", "cyan"),
-        "canceled": colored("[CANCELED]", "red"),
-        'status': colored('STATUS', 'green'),
-        "patient": colored("PATIENT", "magenta")
-    }
+def show_calendars(prompt=None):
+    """
+    This will get all the available slots form the WTC calendar. 
+    """
 
-    return colors
-
-
-def show_code_clinics_calendar():
-    """This function shows the calendar for Code Clinic Calendar. This calendar
-    is global, and can be seen by every WeThinkCode_ student. It returns a list
-    of date, time, meeting summary, the patient email (booked), the volunteer
-    email and the meeting ID."""
-    #service = get_calendar_service()
-    colors = colored_headings()
-    date = date_and_time_str_google()
-
-    events_result = service.events().list(calendarId='wtcteam19jhb@gmail.com',
-        timeMin=date.get('start_day'), timeMax=date.get('end_day'),
-        maxResults=100, singleEvents=True, orderBy='startTime').execute()
-
-    events = events_result.get('items', [])
+    if prompt == "primary":
+        user_name = get_user()[1] 
+        print(f"\n{user_name.title()} Calendar")
+        print_slots(get_primary_calendar())
+    elif prompt == None:
+        print("\nCode Clinics Calendar")
+        print_slots(get_code_clinics_calendar())
     
+    else:
+        command = sys.argv[1]
+        if (len(sys.argv) == 3 and (command == 'slots' or command == "my_cal")
+            and sys.argv[2].isdigit()):
+            get_time_constraints(int(sys.argv[2]))
+        else:
+            command = ""
+            for arg in sys.argv[1:]:
+                command += f"{arg} "
+            print(f"Unrecognized command: \"wtc-cal {command.strip()}\"")
+
+
+
+def print_slots(data):
+    """
+    This function will print out the slots
+    """
+
+    date = colored("DATE", 'green')
+    time = colored("TIME", 'green')
+    summary = colored("SUMMARY", 'cyan')
+    volunteer = colored("VOLUNTEER", 'red')
+    creator = colored("CREATOR", 'red')
+    patient = colored("PATIENT", "magenta")
+    id = colored("ID", "yellow")
+
+    table = PrettyTable()
+
+    if data != []:
+
+        if len(data[0]) == 5:
+            table.field_names = [date, time, summary, creator, id]
+        else:
+            table.field_names = [date, time, summary, patient, 
+                                volunteer, id, "STATUS"]
+            data = formatted_data_output(data)
+    
+        for entry in data:
+            table.add_row(entry)
+        print(table)
+    else:
+        print("EMPTY SHANDIES")
+
+
+def formatted_data_output(data):
+    """
+    This function formats the data to add color to some content when it will be
+    printed out and returns the formated data
+    """
+
+    print_data = []
+
+    if len(data[0]) == 5:
+        for item in data:
+            # Make the summary limited to 12 characters if it exceeds 15
+            info = item[2]
+            event_summary = f"{info[:12]}..." if len(info) > 15 else info
+            slot = [item[0], item[1], event_summary, item[3], item[4]]
+
+            print_data.append(slot)
+            
+        return print_data
+
+
+    for item in data:
+        # Make the summary limited to 12 characters if it exceeds that number
+        info = item[2]
+        event_summary = f"{info[:12]}..." if len(info) > 15 else info
+
+        # Display just the user name instead of user email of the volunteer
+        volunteer = item[4].get('email').split("@")[0]
+
+        # Display just the user name instead of user email of the patient
+        patient = item[3]
+        if patient != "":
+            patient = item[3].split("@")[0]
+        else:
+            patient = item[3]
+
+        # Add color to the status output
+        if item[6] == "[OPEN]":
+            status = colored("[OPEN]", "cyan")
+        elif item[6] == "[BOOKED]":
+            status = colored("[BOOKED]", "red")
+
+        # Return output slot
+        slot = [item[0], item[1], event_summary, patient, volunteer, item[5],
+                status]
+
+        print_data.append(slot)
+    
+    return print_data
+
+
+def get_date_and_time(date_time):
+    """
+    This function takes a datetime object and returns a seperate date and time
+    strings
+    """
+    date_time = parser.parse(date_time)
+    time = date_time.strftime("%H:%M:%S")
+    date = date_time.strftime("%Y-%m-%d")
+
+    return date, time
+
+  
+def get_code_clinics_calendar():
+    """
+    This function returns the code-clinics calendar
+    """
+
+    events_results = get_events_results('wtcteam19jhb@gmail.com')
+    events = events_results.get('items', [])
+
     data = []
     if not events:
-        print("No upcoming events found!")
-
+        pass
+    
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-
-        print(event)
+        date, time = get_date_and_time(start)
 
         if event['status'] == 'tentative':
-            status = colors.get('available')
-            patient = colored('[AVAILABLE]', 'green')
+            status = "[OPEN]"
+            patient = "-"
         elif event['status'] == 'confirmed':
-            status = colors.get('booked')
+            status = "[BOOKED]"
             patient = event['attendees'][0]['email']
 
-        start_date = start.split('T')[0]
-        start_time_UCT = start.split('+')[0].split('T')[1]
-
-        data.append([start_date, start_time_UCT, event['summary'], patient, 
-            event['creator'].get('email'), status, event['id']])
+        # if "func" == event['summary']:
+        #     for item in event.items():
+        #         print(item)
+        data.append([date, time, event['summary'], patient, event['creator'],
+                    event['id'], status, event.get('description')])
 
     return data
 
 
-#def save_volunteer_id
+def get_primary_calendar():
+    """
+    This function returns the primary calendar (user calendar)
+    """
 
+    events_results = get_events_results()
+    events = events_results.get('items', [])
 
-def show_student_calendar():
-    """This function shows the personal student calendar. It's only visible to
-    the logged in student alone. It returns a list of the date, time, the 
-    meeting topic, the creator email and the meeting ID. """
-    #service = get_calendar_service()
-    colors = colored_headings()
-    date = date_and_time_str_google()
-
-    events_result = service.events().list(calendarId='primary',
-        timeMin=date.get('start_day'), timeMax=date.get('end_day'),
-        maxResults=100, singleEvents=True, orderBy='startTime').execute()
-
-    events = events_result.get('items', [])
-
-    student_calendar = []
+    data = []
     if not events:
-        print("No upcoming events found!")
+        pass
 
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
+        date, time = get_date_and_time(start)
 
-        start_date = start.split('T')[0]
-        start_time_UCT = start.split('+')[0].split('T')[1]
+        data.append([date, time, event['summary'],
+                    event['creator'].get('email'), event['id']])
 
-        student_calendar.append([start_date, start_time_UCT, event['summary'],
-            event['creator'].get('email'), event['id']])
-
-    return student_calendar
+    return data
 
 
-def get_student_calendar():
-    """It displays the data of the student in a table form.
-    > Prints out 7 days of content to the terminal."""
-    #service = get_calendar_service()
-    colors = colored_headings()
-    data = show_student_calendar()
-
-    table = PrettyTable()
-    table.field_names = ["DATE", "TIME",
-        "SUMMARY", "CREATOR", "ID"]
-
-    for entry in data:
-        table.add_row(entry)
-
-    print(table)
-
-
-def get_slots_calendar():
-    """It displays the data of the Code Clinic Calendar in a table form.
-    > Prints out 7 days of calendar content to the terminal."""
-    #service = get_calendar_service()
-    colors = colored_headings()
-    data = show_code_clinics_calendar()
-
-    table = PrettyTable()
-    table.field_names = [colors.get("date_header"), colors.get("time_header"),
-        colors.get("summary"), colors.get("patient"), colors.get("volunteer"), 
-        colors.get('status'), colors.get("etag")]
-
-    for entry in data:
-        table.add_row(entry)
-
-    print(table)
-
-# import datetime
-# from calendar_setup import get_calendar_service
-
-
-
-
-# def student_cal():
+def slot_details():
+    """
+    This function list details of the specified slot (Booking or Volunteering)
+    """
     
-#     service = get_calendar_service()
+    if len(sys.argv) != 3:
+        command = ""
+        for arg in sys.argv[1:]:
+            command += f"{arg} "
+        print(f"Unrecognized command: \"wtc-cal {command.strip()}\"")
+    else:
+        id = sys.argv[2].strip()
+        data = load_data()
+    
+        slot = None
+        for item in data:
+            if id == item[5]:
+                slot = item
+                break
 
-#     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-#     print('Getting the upcoming 7 events')
-#     events_result = service.events().list(calendarId='primary', timeMin=now,
-#                                         maxResults=7, singleEvents=True,
-#                                         orderBy='startTime').execute()
-#     events = events_result.get('items', [])
+        if slot == None:
+            print(colored("Slot does not exist.", "red"))
+        else:
+            date = parser.parse(slot[0] +" "+ slot[1]) + timedelta(minutes=30)
+            end = date.strftime("%H:%M")
 
-#     if not events:
-#         print('No upcoming events found.')
-#     for event in events:
-#         start = event['start'].get('dateTime', event['start'].get('date'))
-#         print(start, event['summary'])
+            msg = colored("Slot details:", "yellow")
+            print(f"\n{msg} {item[5]}\n")
+            print("  Creator:\t", item[4].get('email'))
+            print("  Attendee:\t", item[3])
+            print("  Summary:\t", item[2])
+            print("  Description:\t", item[7])
+            print("  Date:\t\t", slot[0])
+            print(f"  Time:\t\t {slot[1][:5]} - {end}\n")
+            # print("\n")
 
-
-# def wtc_clinic_cal():
-
-#     service = get_calendar_service()
-
-#     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-#     print('Getting the upcoming 7 slots')
-#     slots_result = service.events().list(calendarId='wtcteam19jhb@gmail.com', timeMin=now,
-#                                         maxResults=7, singleEvents=True,
-#                                         orderBy='startTime').execute()
-#     slots = slots_result.get('items', [])
-
-#     if not slots:
-#         print('No slot found.')
-#     for slot in slots:
-#         start = slot['start'].get('dateTime', slot['start'].get('date'))
-#         print(start, slot['summary'])
-
-
-# if __name__ == "__main__":
-#     student_cal()
-#     wtc_clinic_cal()
+        
--- codeclinics/login/user_auth.py
+++ codeclinics/login/user_auth.py
@@ -1,10 +1,16 @@
 from os import path
-from calendar_setup import get_calendar_service
-from datetime import datetime
+from calendar_setup.calendar_service import *
+from datetime import datetime, timedelta
 from termcolor import colored
 import sys
 import os
 import json
+from bookings.processing_data import *
+from bookings.list_calendars import get_code_clinics_calendar
+
+# The token file location
+token_path = f'{sys.path[0]}/creds/token.pickle'
+config_path = f"{sys.path[0]}/files/json/.config.json"
 
 
 def get_time_date():
@@ -26,11 +32,9 @@ def get_time_date():
 def get_user_details():
     """Retrieving user details from the calendar setup module."""
 
-    # Fetching the Google Calendar API
-    service = get_calendar_service()
-    events_result = service.events().list(calendarId='primary').execute()
+    events_results = get_events_results()
     
-    user_email = events_result.get('summary')
+    user_email = events_results.get('summary')
     username = user_email.split('@')[0]
     date_and_time = get_time_date()
 
@@ -48,19 +52,16 @@ def remove_token():
     """ Removing the token file if the user is not logged in or if the token
     file exists.""" 
 
-    try:
-        os.remove(path.realpath("token.pickle"))
-    except FileNotFoundError:
-        pass
+    is_found = True
 
-def get_login_state():
-    """This function checks whether the user is logged in or not.
-        > Returns a boolean."""
-    token_path = f"{sys.path[0]}/token.pickle"
-    if path.exists(token_path):
-        return True
-    else:
-        return False
+    try:
+        os.remove(token_path)
+    except FileNotFoundError:
+        is_found = False
+        print("You already loggged out!")
+        print("\nPlease run: \"wtc-cal login\"\n")
+
+    return is_found
 
 
 def validate_email(user_email):
@@ -72,44 +73,34 @@ def validate_email(user_email):
     domain_name = user_email.split('@')[1]
 
     if domain_name.find("wethinkcode.co.za") > -1:
-        print(f'Welcome {str(user_name).capitalize()}.')
+        msg = colored(f"Welcome {str(user_name).capitalize()}!", "green")
+        print(msg)
     else:
-        os.remove(path.realpath('token.pickle'))
+        remove_token()
         print("Invalid email address!")
 
 
-def writing_to_json_file(user_details):
+def writing_to_json_file():
     """Writing user data to a hidden .config file."""
 
     user_details = get_user_details()
 
-    with open('.config.json', 'w') as write_config:
-        json.dump(user_details, write_config)
-
-
-def writing_to_a_txt(current_user):
-    """ Writing the current user email address to a .txt file"""
-
-    user_file_txt = get_user_details()
-    current_user = user_file_txt.get('email')
-
-    current_logged_in = open('current_login.txt', 'w')
-    current_logged_in.write(current_user)
-    current_logged_in.close()
+    with open(config_path, 'w') as write_config:
+        json.dump(user_details, write_config, indent=4)
 
 
 def get_user_status():
     """Getting the status of the signed in user. If they're signed in, it should
     print out that the user is logged in, if not, then it should instructions to
     the user about loggin in."""
+
+    if path.exists(token_path):
     
-    if path.exists(sys.path[0]+'/token.pickle'):
-        current_user = open(os.path.abspath(sys.path[0]+"/current_login.txt"), 'r')
-        user_email = current_user.readline()
-        current_user.close()
+        user_email = get_user_email()   
+
         connected = colored('[CONNECTED]', 'green')
         print(connected + " Google Calendar | Code Clinic Booking System") 
-        print(f"Signed in as {user_email}.")
+        print(f"Signed in as {user_email}")
     else:
         offline = colored('[OFFLINE]', 'red')
         print(offline + "\nPlease run: \"wtc-cal login\"")
@@ -118,29 +109,95 @@ def get_user_status():
 def user_login():
     """Signing the user by redirecting them to the sign in page. If they are
     logged in, print out a statemet. If they're not, create a token file for
-    them."""
-  
-    if get_login_state() == False:
-        remove_token()
+    them."""        
 
-        user_details = get_user_details()
-        user_email = user_details.get("email")
+    if not path.exists(token_path):
+        writing_to_json_file()
 
-        if not path.exists("../token.pickle"):
-            validate_email(user_email)
-            writing_to_json_file(user_details)
-            writing_to_a_txt(user_email)
-        else:
-            print("You are already logged in!")
+        user_email = get_user_email()
+        os.system('clear')
+        validate_email(user_email)
     else:
         print("You are already logged in!")
+
 
 def user_logout():
     """Loggin out the user from the booking sysem."""
 
-    try:
-        os.remove(path.abspath(sys.path[0]+"/token.pickle"))
-        print("You have been logged out from the system!")
-    except FileNotFoundError:
-        print("You already loggged out!")
-        print("Please run: \"wtc-cal login\"")     
+    if remove_token():
+        print(colored("You have successfully logged out!", "yellow"))
+
+
+def get_login_state():
+    """This function checks whether the user is logged in or not.
+        > Returns a boolean."""
+
+    if path.exists(token_path):
+        return True
+    else:
+        return False
+
+
+def show_config():
+    """
+    This function displays the config file
+    """
+
+    config = None
+    if os.path.exists(config_path):
+        with open(config_path, 'r') as json_file:
+            config = json.load(json_file)
+
+        print(f"Reading config from {sys.path[0]}/.config.json\n")
+        print("Config {")
+        print(f"    editor: wtc-cal")
+        print(f"    repo_path: \"{sys.path[0]}\"")
+        print(f"    username: \"{config.get('email')}\"")
+        print("    code_clinics_manager: \"wtcteam19jhb@gmail.co.za\"")
+        print("}")
+    else:
+        print("Config file does not exist")
+   
+
+def get_user_email():
+    """Getting the data from the json file"""
+
+    user_details = None
+
+    with open(config_path, 'r') as json_file:
+        user_details = json.load(json_file)
+
+    user_email = user_details.get('email')
+
+    return user_email
+
+
+def auto_logout():
+    """
+    This function checks if your token is still valid
+    """
+    
+    if path.exists(token_path):
+    
+        data = None
+
+        with open(config_path, 'r') as json_file:
+            data = json.load(json_file)
+    
+        # Parse type string date and time objects to integer type objects
+        hours = int(data['time'][:2])
+        mins = int(data['time'][3:5])
+        mon = int(data['date'][:2])
+        day = int(data['date'][3:5])
+        year = int('20'+data['date'][6:8])
+        
+        #logout_time = date + timedelta(minutes=30)
+        date = datetime(year=year, month=mon, day=day, hour=hours, minute=mins)    
+        logout_time = date + timedelta(hours=1)
+        
+        if(datetime.now() > logout_time):
+            save_data(get_code_clinics_calendar())
+            remove_token()
+            return True
+
+    return False   
